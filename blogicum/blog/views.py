3bf33063059a9e156aca0f8bf18b43043664adb9
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-07-22 18:01:36 +0300

    Исправления после принятия проекта
    Верссия 1.2.0 > 1.3.0 (Дополненная)
    22/07/2024
    
        Запросы к БД сделаны через customQuerySets
        View функции были оптимизированы
        Core модели поправлены
        Классы-наследники обновлены

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,21 +2,16 @@ from django.shortcuts import get_object_or_404, render
 
 from blog.models import Category, Post
 
-from blog.models import Category, Post
-
-
 DISP_LIMIT = 5
 
 
 def index(request):
-    context = {'post_list': Post.posts()[:DISP_LIMIT]}
     context = {'post_list': Post.posts()[:DISP_LIMIT]}
     template = 'blog/index.html'
     return render(request, template, context)
 
 
 def post_detail(request, id):
-    context = {'post': get_object_or_404(Post.posts(), pk=id)}
     context = {'post': get_object_or_404(Post.posts(), pk=id)}
     template = 'blog/detail.html'
     return render(request, template, context)
